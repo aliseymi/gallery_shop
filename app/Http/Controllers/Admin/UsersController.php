59cef6b7b3d7838b3522e55040f85cee2a7b3d04
seymi <seyedali1377mir@gmail.com>
2021-12-04 13:53:05 +0330

    showing all users implemented

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function all()
+    {
+        $users = User::paginate(10);
+
+        return view('admin.users.all', compact('users'));
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function edit()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+
+    public function delete()
+    {
+        
+    }
+}
